f49733969b56e0f63a7e693ec603765793ba993d
Vivian Yim <keroviv86@gmail.com>
2021-12-14 21:09:24 -0600

    commit tree 9d9b7c330ee36cc845570f0ebf41796392ac9151

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+function findMatching(drivers, string){
+    let list =[]
+    for(let i=0; i < drivers.length; i++){
+        if(drivers[i]=== string || drivers[i]===string.toLowerCase()){
+            list.push(drivers[i])
+        }
+    }
+    return list
+    
+
+}
+
+// function findMatching(drivers, string){
+//     let answer = drivers.filter(element => element === string || element === string.toLowerCase())
+//     return answer
+// }
+
+
+function fuzzyMatch(drivers, string){
+    let firstLetter = drivers.filter(driver => driver.slice(0, string.length)=== string)
+    return firstLetter
+}
+
+
+function matchName(drivers, string){
+    let name = drivers.filter(driver => driver.name === string)
+    return name
+
+}
